eb35eeafbb717712cc361010aa0340d961977165
n37r06u3 <n37r06u3@gmail.com>
2016-09-04 11:23:50 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import {HeaderComponent} from './header'
 @Component({
   moduleId: module.id,
   selector: 'rb-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives:[HeaderComponent]
 })
 export class AppComponent {
   title = 'app works!';
