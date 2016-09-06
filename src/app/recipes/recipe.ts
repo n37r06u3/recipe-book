96b465f54c2ab255b5fbb317644d2e6d5ea61aa3
n37r06u3 <n37r06u3@gmail.com>
2016-09-06 14:00:42 +0800

    add shopping service

--- src/app/recipes/recipe.ts
+++ src/app/recipes/recipe.ts
@@ -1,6 +1,6 @@
 import {Ingredient} from "../shared/ingredient";
 export class Recipe {
 
-  constructor(public name, public desc, public imgPath, public ingredients: Ingredient[]){}
+  constructor(public name:string, public desc:string, public imgPath:string, public ingredients: Ingredient[]){}
 
 }
